1890bae608d31339623c0d396e0b6495cc5ccb7e
Carballo Alamo Francisco Manuel <97671162+franciscoBanderas@users.noreply.github.com>
2022-12-12 20:19:15 +0100

    Ejercicio1 js

---
+++ ejercicio1.js
@@ -0,0 +1,15 @@
+"use strict";
+
+//Seleccionamos cada elemento por su id que se nos proporciona en el código html
+
+document.getElementById("cuerpo-tabla");
+
+document.getElementById("producto");
+
+document.getElementById("cantidad");
+
+document.getElementById("precio-unitario");
+
+document.getElementById("agregarLinea");
+
+document.getElementById("descuento");
